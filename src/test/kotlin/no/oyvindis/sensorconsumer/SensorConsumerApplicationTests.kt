349c7a8d0d961c709ce0dc2e9a8d465e4a5d18da
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-09-01 10:58:33 +0200

    init

---
+++ src/test/kotlin/no/oyvindis/sensorconsumer/SensorConsumerApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.oyvindis.sensorconsumer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SensorConsumerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
